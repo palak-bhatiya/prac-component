bb75ad21f450b68d710175369e3196c0e12a0824
palak-bhatiya <git14hub@gmail.com>
2022-12-06 16:20:22 +0530

    component

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header';
+import Container from './Container';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Container/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Container.js
@@ -0,0 +1,8 @@
+function Container(){
+    return(
+        <>
+            <h1>Container</h1>
+        </>
+    )
+}
+export default Container;
